3e1f2819b1405ab060af4229b56227a142d14bdc
Brett Rowberry <brett.rowberry@gmail.com>
2015-03-16 14:58:35 -0500

    Trying to get workable state.

--- rdtReceiver.c
+++ rdtReceiver.c
@@ -27,6 +27,8 @@ int ServerSocket = 0;
 struct hostent * HostByName = NULL;
 struct sockaddr_in ServerAddress;
 
+const char* ServerShutdownMessage = "Server shutting down...";
+
 //Function prototypes
 int validateAndPrintPort(int numberOfArgs, const char * nameOfProgram, const char * portString);
 void ExitOnError(char * errorMessage);
@@ -34,6 +36,16 @@ void BindSocket();
 void OpenSocket(int port);
 void InitAddressStruct(int port);
 void DisplayServerInfo();
+void AcceptConnections();
+void DisplayInfo();
+void HandleClientRequests(struct sockaddr_in* clientAddress);
+int XMLParser(  const char* beginXml,
+        const char* endXml,
+        char* clientMessage,
+        char* token,
+        int tokenSize);
+void ParseClientMessage(char* clientMessage,  struct sockaddr_in* clientAddress, int clientSocket);
+
 
 int main(int argc, const char* argv[])
 {
@@ -41,6 +53,8 @@ int main(int argc, const char* argv[])
 //    if(port == -1) exit(1);
 
     OpenSocket(port);
+
+
 
 //    char message[SEGMENT_SIZE];
 //    sprintf(message, "%s", receiveMessage(socketFD));
@@ -140,6 +154,87 @@ void ExitOnError(char* errorMessage)
     exit(1);
 }
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   AcceptConnections
+    Accepts the client connection and creates a new detached thread for the client.
+    @return           --    void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void AcceptConnections()
+{
+    /*~~~~~~~~~~~~~~~~~~~~~Local vars~~~~~~~~~~~~~~~~~~~~~*/
+    struct sockaddr_in clientAddress;
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    DisplayInfo();
+    printf("Waiting for connection... ");
+    fflush(stdout);
+    for(;;)
+    {
+        HandleClientRequests(&clientAddress);
+    }
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   HandleClientRequests
+    Depending on the string received from the client, we either print that it failed or parse
+    the message, then close the socket and free the thread.
+    @param  ClientSocketPtr -- A pointer to the client socket typecasted to a void*
+    @return           -- void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void HandleClientRequests(struct sockaddr_in* clientAddress)
+{
+    fflush(stdout);
+    /*~~~~~~~~~~~~~~~~~~~~~Local vars~~~~~~~~~~~~~~~~~~~~~*/
+    char stringBuffer[SEGMENT_SIZE];
+    bzero(stringBuffer, SEGMENT_SIZE);
+    socklen_t clientAddressLength = sizeof(struct sockaddr_in);
+    socklen_t bufSize = SEGMENT_SIZE;
+    int length;
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    fflush(stdout);
+    length = recvfrom(
+            ServerSocket,                     //Server socket
+            stringBuffer,                     //Buffer for message
+            bufSize,             //Size of buffer
+            0,                                //Flags
+            (struct sockaddr*)clientAddress,  //Source address
+            &clientAddressLength              //Size of source address
+    );
+    stringBuffer[length] = '\0';
+    puts("");
+    printf("Client IP: %s\n", inet_ntoa(clientAddress->sin_addr));
+//    printf("Client port: %hu\n", ntohs(clientAddress->sin_port));
+    printf("Received message: %s\n", stringBuffer);
+    ParseClientMessage(stringBuffer, clientAddress, length);
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   DisplayInfo
+    Displays the connection info of the server.
+    @return           --    void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void DisplayInfo()
+{
+    int i = 0;
+    struct in_addr ipAddress;
+    struct sockaddr_in sockIn;
+    int sockLen = sizeof(sockLen);
+    getsockname(ServerSocket, (struct sockaddr*)&sockIn, (socklen_t*)&sockLen );
+    printf("Host Name: %s\n",HostByName->h_name);
+    printf("IP:        ");
+    while(HostByName->h_addr_list[i] != 0)
+    {
+        ipAddress.s_addr = *(u_long*)HostByName->h_addr_list[i++];
+        printf("%s\n", inet_ntoa(ipAddress));
+    }
+    printf("Port:      %d\n", htons(sockIn.sin_port));
+    fflush(stdout);
+}
+
 /*
  * Receives a message from an RDT sender on a specified port.
  *
@@ -150,4 +245,138 @@ void ExitOnError(char* errorMessage)
 char * receiveMessage (int sockFD)
 {
     return NULL;
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   ParseClientMessage
+    Handles the message for the client and sends a message back to the client
+    @param  clientMessage        -- Pointer to message received by client
+    @param  ClientSocket         -- The socket to the client
+    @return                      -- void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void ParseClientMessage(char* clientMessage,  struct sockaddr_in* clientAddress, int clientSocket)
+{
+    /*~~~~~~~~~~~~~~~~~~~~~Local vars~~~~~~~~~~~~~~~~~~~~~*/
+    int i = 0;
+    char string[SEGMENT_SIZE]; //String to send back to client
+    char token[SEGMENT_SIZE];  //Token to use for echo reply
+    string[0] = '\0';
+    const int NUMLOADAVG = 3; //Number of load averages queries
+    socklen_t clientAddressLength = sizeof(struct sockaddr_in);
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    /*~~~~~~~~~~~~~~~~~~~~~Load avg response~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    if(strcmp(clientMessage, "<loadavg/>") == 0)
+    {
+        double loadavg[NUMLOADAVG];
+        getloadavg(loadavg, NUMLOADAVG); //Get load average and put in double array
+
+        //Begin string format:
+        strcat(string, "<replyLoadAvg>");
+        for(i = 0; i < NUMLOADAVG; i++)
+        {
+            char tempAvg[SEGMENT_SIZE];
+            sprintf(tempAvg, "%lf:", loadavg[i]);
+            strcat(string, tempAvg);              //Append string to string we return to client
+        }
+        strcat(string, "</replyLoadAvg>\0"); //End of string
+    }
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        /*~~~~~~~~~~~~~~~~~~~~~Echo response~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    else if ((XMLParser("<echo>", "</echo>", clientMessage, token, sizeof(token))) == 1)
+    {
+        //Set return echo string
+        strcat(string, "<reply>");
+        strcat(string, token);
+        strcat(string, "</reply>\0");
+    }
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        /*~~~~~~~~~~~~~~~~~~~~~Shut down response~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    else if(strcmp(clientMessage, "<shutdown/>") == 0)
+    {
+        strcat(string, ServerShutdownMessage);
+    }
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    else //Else we have an invalid format
+        strcat(string, "<error>unknown format</error>\0");
+    printf("Sending back %s\n\n", string);
+    if(     (sendto(
+            ServerSocket,                       //Client socket
+            string,                             //String buffer to send to client
+            strlen(string),                     //Length of buffer
+            0,                                  //flags
+            (struct sockaddr*)clientAddress,    //Destination
+            clientAddressLength                 //Length of clientAddress
+
+    )) < 0) //If sendto fails
+        printf("Failed to send\n");
+    if(strcmp(string, ServerShutdownMessage) == 0) //If server shutdown message received
+    {
+        close(ServerSocket);
+        exit(1);
+    }
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION: XMLParser
+    Parses an XML value and returns a token.
+    @param begin           --      The expected beginning of an XML expression
+    @param end             --      The expected ending of an XML expression
+    @param token           --      The token extracted from the expression
+    @param clientMessage   --      Message to parse
+    @param length          --      Size of token
+    @return                --      1 on success, 0 on failure, -1 if token is too large to fit
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+int XMLParser(  const char* beginXml,
+        const char* endXml,
+        char* clientMessage,
+        char* token,
+        int tokenSize)
+{
+    //~~~~~~~~~~~~~Local vars ~~~~~~~~~~~~~~~~~~~~~~~//
+    char tempString[strlen(clientMessage)];
+    char *delimiter = NULL;
+    int returnVal = 0;
+    int i = 0;
+    int foundDelimiter = 0;
+    int beginXmlLength = strlen(beginXml);
+    int endXmlLength = strlen(endXml);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+    token[0] = '\0';
+    memcpy(tempString, clientMessage, beginXmlLength); //Copy first part of clientMessage into temp for comparison.
+    tempString[beginXmlLength] = '\0';
+    if(strcmp(tempString, beginXml) == 0 ) //If beginXml is found
+    {
+        memcpy(tempString, clientMessage, strlen(clientMessage)); //Copy entire clientMessage
+        for(i = 1; i < strlen(clientMessage); i++) //Check for valid delimiter here
+        {
+            if(tempString[i] == '<')
+            {
+                delimiter = tempString+i;//Potential valid delimiter found. Point delimiter ptr to location.
+                foundDelimiter = 1;
+                break;
+            }
+        }
+        if(foundDelimiter)
+        {
+            delimiter[endXmlLength] = '\0';//Set end of delimiter to null
+            if (strcmp(delimiter, endXml) != 0) //If invalid delimiter
+                returnVal = 0;
+            else {
+                returnVal = 1;//Set valid return
+                char *tempToken = clientMessage + (strlen(beginXml)); //Set temporary token to end of starting delimiter
+                strtok(tempToken, "<");
+                if (strlen(tempToken) > tokenSize) returnVal = -1;              //If token is too large, return -1
+                else if (strcmp(tempToken, endXml) == 0) token[0] = '\0';       //Else if empty token found
+                else strcat(token, tempToken);                                  //Else put extracted token in variable
+            }
+        }
+    }
+    return returnVal;
 }
